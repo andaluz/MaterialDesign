187d8d951a807e70d26f1a45b155a64b7566d257
Nordin Mobile Dev <lidmaatschap@gmail.com>
2016-02-16 12:13:55 +0100

    Added custom toolbar in transparent fragment.

--- app/src/main/java/app/appplanet/com/mytoolbar/TransparentFragment.java
+++ app/src/main/java/app/appplanet/com/mytoolbar/TransparentFragment.java
@@ -2,11 +2,14 @@ package app.appplanet.com.mytoolbar;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,8 +34,27 @@ public class TransparentFragment extends Fragment {
     }
 
     private void modifyToolbar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            //
+        } else {
+            //
+        }
         //((AppCompatActivity)getActivity()).getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#80000000")));
-        //((AppCompatActivity)getActivity()).getSupportActionBar().
+        //((AppCompatActivity)getActivity()).getSupportActionBar().setBackgroundDrawable(new Drawable() {
+        //});
+        Toolbar toolbar = (Toolbar)((AppCompatActivity) getActivity()).findViewById(R.id.toolbar);
+        //toolbar.setBackgroundResource(android.R.color.transparent);
+        toolbar.setVisibility(View.GONE);
+
+        //Toolbar tb = new Toolbar(getActivity());
+        //tb.setBackgroundResource(android.R.color.transparent);
     }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        Toolbar toolbar = (Toolbar)((AppCompatActivity) getActivity()).findViewById(R.id.toolbar);
+        toolbar.setVisibility(View.VISIBLE);
+    }
 }
