7b88d0a40c079799c529f82f73edb2f9e5d3c4f1
Nordin Mobile Dev <lidmaatschap@gmail.com>
2016-07-04 17:04:31 +0200

    Added some CursorLoader snippets.

--- app/src/main/java/app/appplanet/com/mytoolbar/LoaderFragment.java
+++ app/src/main/java/app/appplanet/com/mytoolbar/LoaderFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,16 +16,42 @@ import android.view.ViewGroup;
  */
 public class LoaderFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
+    // Identifies a particular Loader being used in this component
+    private static final int URL_LOADER = 10;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        /*
+         * Initializes the CursorLoader. The URL_LOADER value is eventually passed
+         * to onCreateLoader().
+         */
+        getLoaderManager().initLoader(URL_LOADER, null, this);
+
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
-
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+        /*
+         * Takes action based on the ID of the Loader that's being created
+         */
+        switch (id) {
+            case URL_LOADER:
+                // Returns a new CursorLoader
+                return new CursorLoader(
+                        getActivity(),  // Parent activity context
+                        null,           // Table to query
+                        null,           // Projection to return
+                        null,           // No selection clause
+                        null,           // No selection arguments
+                        null            // Default sort order
+                );
+            default:
+                // An invalid id was passed in
+                return null;
+        }
     }
 
     @Override
